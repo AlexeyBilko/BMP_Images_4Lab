174dc3d9cecd6238ca232eebf8d8b542f5494533
Alexey Bilko <bilkoalexey67@gmail.com>
2021-04-18 15:31:29 +0300

    Add project files.

---
+++ BMP_images/Pixel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMP_images
+{
+    public class Pixel
+    {
+        public uint Green { get; set; }
+        public uint Blue { get; set; }
+        public uint Red { get; set; }
+
+        public Pixel()
+        {
+            Green = 0;
+            Blue = 0;
+            Red = 0;
+        }
+
+        public Pixel(int g, int b, int r)
+        {
+            Green = (uint)g;
+            Blue = (uint)b;
+            Red = (uint)r;
+        }
+
+        public void ChangeColors(int g, int b, int r)
+        {
+            Green += (uint)g;
+            Blue += (uint)b;
+            Red += (uint)r;
+        }
+    }
+}
---
+++ BMP_images/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMP_images
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
